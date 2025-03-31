386f7e43c7ac067712ae6ca8a920dc54b800b7f1
syjoy1993 <syjoy1993@gmail.com>
2025-04-01 02:45:00 +0900

    test

--- src/test/java/saya/webhook/WebhookApplicationTests.java
+++ src/test/java/saya/webhook/WebhookApplicationTests.java
@@ -2,8 +2,10 @@ package saya.webhook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-secret.properties") // ✅ 요거 한 줄!
 class WebhookApplicationTests {
 
     @Test
